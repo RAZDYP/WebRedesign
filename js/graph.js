70024ea870e4ad6048a0f70e1fc93cc54ea7dbcc
NoisyBotDude <ashishsarmah11@gmail.com>
2022-08-10 10:43:01 +0530

    feat: modularized all the graphs
    Co-authored-by: rajdeeprajdeep03@gmail.com <rajdeeprajdeep03@gmail.com>

---
+++ js/graph.js
@@ -0,0 +1,312 @@
+// Chart 1
+
+const ctx1 = document.getElementById('myChart1').getContext('2d');
+const myChart1 = new Chart(ctx1, {
+    type: 'line',
+    data: {
+        labels: ['', '', '', '', '', ''],
+        datasets: [{
+            label: '',
+            data: [5, 7, 6, 4, 5, 3],
+            fill: true,
+            tension: 0.2,
+            backgroundColor: 'rgba(0, 0, 255, 0.4',
+            borderColor: 'rgba(0, 0, 255, 1)',
+            pointRadius: 0,
+            pointHoverBackgroundColor: 'rgb(255, 99, 132)',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
+            pointHoverRadius: 0,
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            y: {
+                display: false,
+                beginAtZero: true,
+                max: 10,
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
+
+
+// Chart 2
+
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+const myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['', '', '', '', '', ''],
+        datasets: [{
+            label: '',
+            data: [5, 7, 6, 4, 5, 3],
+            fill: true,
+            tension: 0.2,
+            backgroundColor: 'rgba(0, 255, 0, 0.4)',
+            borderColor: 'rgba(0, 255, 0, 1)',
+            pointRadius: 0,
+            pointHoverBackgroundColor: 'rgb(255, 99, 132)',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
+            pointHoverRadius: 0,
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            y: {
+                display: false,
+                beginAtZero: true,
+                max: 10,
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
+
+// Chart 3
+
+const ctx3 = document.getElementById('myChart3').getContext('2d');
+const myChart3 = new Chart(ctx3, {
+    type: 'line',
+    data: {
+        labels: ['', '', '', '', '', ''],
+        datasets: [{
+            label: '',
+            data: [5, 7, 6, 4, 5, 3],
+            fill: true,
+            tension: 0.2,
+            backgroundColor: 'rgba(0, 255, 0, 0.4)',
+            borderColor: 'rgba(0, 255, 0, 1)',
+            pointRadius: 0,
+            pointHoverBackgroundColor: 'rgb(255, 99, 132)',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
+            pointHoverRadius: 0,
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            y: {
+                display: false,
+                beginAtZero: true,
+                max: 10,
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
+
+// Chart 4
+
+const ctx4 = document.getElementById('myChart4').getContext('2d');
+const myChart4 = new Chart(ctx4, {
+    type: 'line',
+    data: {
+        labels: ['', '', '', '', '', ''],
+        datasets: [{
+            label: '',
+            data: [5, 7, 6, 4, 5, 3],
+            fill: true,
+            tension: 0.2,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            pointRadius: 0,
+            pointHoverBackgroundColor: 'rgb(255, 99, 132)',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
+            pointHoverRadius: 0,
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            y: {
+                display: false,
+                beginAtZero: true,
+                max: 10,
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
+
+// Chart 5
+
+const ctx5 = document.getElementById('myChart5').getContext('2d');
+const myChart5 = new Chart(ctx5, {
+    type: 'line',
+    data: {
+        labels: ['', '', '', '', '', ''],
+        datasets: [{
+            label: '',
+            data: [5, 7, 6, 4, 5, 3],
+            fill: true,
+            tension: 0.2,
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            pointRadius: 0,
+            pointHoverBackgroundColor: 'rgb(255, 99, 132)',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
+            pointHoverRadius: 0,
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            y: {
+                display: false,
+                beginAtZero: true,
+                max: 10,
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
+
+// Chart 6
+
+const ctx6 = document.getElementById('myChart6').getContext('2d');
+const myChart6 = new Chart(ctx6, {
+    type: 'line',
+    data: {
+        labels: ['', '', '', '', '', ''],
+        datasets: [{
+            label: '',
+            data: [5, 7, 6, 4, 5, 3],
+            fill: true,
+            tension: 0.2,
+            backgroundColor: 'rgba(0, 0, 200, 0.2',
+            borderColor: 'rgba(0, 0, 255, 1)',
+            pointRadius: 0,
+            pointHoverBackgroundColor: 'rgb(255, 99, 132)',
+            pointHoverBorderColor: 'rgb(255, 99, 132)',
+            pointHoverRadius: 0,
+            borderWidth: 1,
+        }]
+    },
+    options: {
+        responsive: true,
+        interaction: {
+            mode: 'index',
+            intersect: false
+        },
+        scales: {
+            y: {
+                display: false,
+                beginAtZero: true,
+                max: 10,
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            x: {
+                display: false,
+                grid: {
+                    display: false
+                }
+            },
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
