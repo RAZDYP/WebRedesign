241dc522c6d0d58310b46b591f4a733ad6900936
NoisyBotDude <ashishsarmah11@gmail.com>
2022-08-11 23:58:19 +0530

    feat: added new input field in groups
    Co-authored-by: rajdeeprajdeep03@gmail.com <rajdeeprajdeep03@gmail.com>

--- js/pick.js
+++ js/pick.js
@@ -4,12 +4,24 @@ var contactsMessage = document.getElementById("message-contacts");
 var manualRadio = document.getElementById("manual");
 var manualMessage = document.getElementById("message-manual");
 
+var groupsRadio = document.getElementById("groups");
+var groupsMessage = document.getElementById("message-groups");
+console.log(groupsMessage);
+
 contactsRadio.addEventListener("click", function() {
     contactsMessage.classList.remove("hide");
     manualMessage.classList.add("hide");
+    groupsMessage.classList.add("hide");
 })  
 
 manualRadio.addEventListener("click", function() {
     contactsMessage.classList.add("hide");
     manualMessage.classList.remove("hide");
+    groupsMessage.classList.add("hide");
+})
+
+groupsRadio.addEventListener("click", function() {
+    contactsMessage.classList.add("hide");
+    manualMessage.classList.add("hide");
+    groupsMessage.classList.remove("hide");
 })
