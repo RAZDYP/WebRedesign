e911efa085fa27e951dd5e62f854b405878f1638
NoisyBotDude <ashishsarmah11@gmail.com>
2022-08-21 15:38:30 +0530

    feat: added javascript to navbaar

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,11 @@
 var dashboardButton = document.getElementsByClassName("dashboard-navbaar--links--button");
+var dashboardContent = document.getElementsByClassName("dashboard-content-info");
 
 $(dashboardButton).click(function(e){
     e.preventDefault();
     $(this).parent().find('button.active').removeClass('active');
     $(this).addClass("active");
+    $(dashboardContent).parent().find('div.dashboard-content-info').addClass("hide");
+    var buttonId = $(this).attr('id')
+    $('.' + buttonId).removeClass("hide");
 });
